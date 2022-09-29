a2c3ab49e470e40ab2cf09878a913243e5230a3a
JuliaGord <gordiychenko2work@gmail.com>
2022-09-29 23:43:57 +0300

    Update ThirdTask

--- Branching/ThirdTask.java
+++ Branching/ThirdTask.java
@@ -12,9 +12,9 @@ class ThirdTask {
         int c = scanner.nextInt();
         int d = b * b - 4 * a * c;
         if (d > 0) {
-            double x1 = ((double)(-b) + Math.sqrt((double)d)) / (double)(2 * a);
-            double x2 = ((double)(-b) - Math.sqrt((double)d)) / (double)(2 * a);
-            System.out.println("x1 = " + x1 + "\nx2 = " + x2);
+            double x1 = ((-b) + Math.sqrt(d)) / (2 * a);
+            double x2 = ((-b) - Math.sqrt(d)) / (2 * a);
+            System.out.println("x1 = " + x1 + "\n" + "x2 = " + x2);
         } else if (d == 0) {
             int x1 = -b / (2 * a);
             System.out.println("x1, x2 = " + x1);
