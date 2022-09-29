69e12e18997558c4489fea12eac47cce392a8fd8
JuliaGord <gordiychenko2work@gmail.com>
2022-09-30 00:07:48 +0300

    Update SecondTask

--- Branching/SecondTask.java
+++ Branching/SecondTask.java
@@ -14,31 +14,17 @@ class SecondTask {
                 System.out.println("У вас 1 гривня");
                 break;
             case 2:
-                System.out.println("У вас 2 гривні");
-                break;
             case 3:
-                System.out.println("У вас 3 гривні");
-                break;
             case 4:
-                System.out.println("У вас 4 гривні");
+                System.out.println("У вас " + number + " гривні");
                 break;
             case 5:
-                System.out.println("У вас 5 гривень");
-                break;
             case 6:
-                System.out.println("У вас 6 гривень");
-                break;
             case 7:
-                System.out.println("У вас 7 гривень");
-                break;
             case 8:
-                System.out.println("У вас 8 гривень");
-                break;
             case 9:
-                System.out.println("У вас 9 гривень");
-                break;
             case 10:
-                System.out.println("У вас 10 гривень");
+                System.out.println("У вас " + number + " гривень");
                 break;
             default:
                 System.out.println("Ви ввели неправильне число.");
